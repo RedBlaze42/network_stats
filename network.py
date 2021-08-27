0d38dcc1bfaa3ba140e65c0b1a80e046a6eab647
RedBlaze42 <allairehugo@gmail.com>
2021-08-27 04:47:13 +0200

    Continue relation computing if author has only on sub

--- network.py
+++ network.py
@@ -167,7 +167,7 @@ class RedditNetwork():
             for author in authors:
                 progress_bar.update(1)
                 author_name, author_data = list(author.items())[0]
-                if len(author_data) > 3000: continue #Ignore les auteurs avec un commentaires dans plus de 3000 sub différents (souvent des bots)
+                if len(author_data) <= 1 or len(author_data) > 3000: continue #Ignore les auteurs avec un commentaires dans plus de 3000 sub différents (souvent des bots)
                 
                 if author_name in self.blacklisted_authors: continue
                 
